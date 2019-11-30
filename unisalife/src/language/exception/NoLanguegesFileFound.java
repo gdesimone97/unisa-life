aaecbf1f1afde7ca2974a9a0e01ddf631492bcbb
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-11-30 16:15:26 +0100

    Revert "Revert "Create NoLanguegesFileFound.java""
    This reverts commit c52957da87265fd0ae75b36ca23f19b7a36495bb.

---
+++ unisalife/src/language/exception/NoLanguegesFileFound.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package language.exception;
+
+/**
+ *
+ * @author desio
+ */
+public class NoLanguegesFileFound extends Exception {
+
+    /**
+     * Creates a new instance of <code>NoLanguegesFileFound</code> without
+     * detail message.
+     */
+    public NoLanguegesFileFound() {
+    }
+
+    /**
+     * Constructs an instance of <code>NoLanguegesFileFound</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public NoLanguegesFileFound(String msg) {
+        super(msg);
+    }
+}
