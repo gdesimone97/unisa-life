f573924491385a329f85e25228b7f62f6100d8f8
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-12-01 22:20:59 +0100

    Update TextManager.java

--- unisalife/src/language/TextManager.java
+++ unisalife/src/language/TextManager.java
@@ -11,21 +11,34 @@ import language.exceptions.*;
 
 /**
  * Abstract class where are defined the main methods that a class have to have
- * to get the available languages of the game and the current one,
- * set the language of the game and obtain a set of string respect to a game's object.
+ * to get the available languages of the game and the current one, set the
+ * language of the game and obtain a set of strings respect to a game's object.
+ *
  * @author Giuseppe De Simone
  */
 public abstract class TextManager extends LanguageManager implements TextFinder {
 
+    /**
+     * {@inheritDoc }
+     */
     @Override
     public abstract Set<String> getAvailableLanguages();
 
+    /**
+     * {@inheritDoc }
+     */
     @Override
     public abstract String getCurrentLanguage();
 
+    /**
+     * {@inheritDoc }
+     */
     @Override
     public abstract void setLanguage(String lang) throws LanguageSelectedNotAvailableException;
 
+    /**
+     * {@inheritDoc }
+     */
     @Override
     public abstract List<String> getString() throws StringNotFoundException;
 }
