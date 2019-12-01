13281091c1851dccff287242fc2485b190995e57
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-12-01 22:20:54 +0100

    Update LanguageManager.java

--- unisalife/src/language/LanguageManager.java
+++ unisalife/src/language/LanguageManager.java
@@ -9,6 +9,9 @@ import java.util.Set;
 import language.exceptions.*;
 
 /**
+ * Abstract class that allow to developer to get the current language and set
+ * one. This class define also the interface to obtain all game's languages
+ * respect to selected languages data source
  *
  * @author Giuseppe De Simone
  */
@@ -16,13 +19,30 @@ abstract class LanguageManager {
 
     private String currentLanguage;
 
+    /**
+     * Method to obtain all available game's languages
+     *
+     * @return a set of strings containing all games languages
+     */
     public abstract Set<String> getAvailableLanguages();
 
+    /**
+     * Method to get the set language
+     *
+     * @return a string containing the current game's language
+     */
     public String getCurrentLanguage() {
         return currentLanguage;
     }
 
-    public void setLanguage(String lang) throws LanguageSelectedNotAvailableException{
+    /**
+     * Set the game's language compared to the language given as parameter
+     *
+     * @param lang - language to set
+     * @throws LanguageSelectedNotAvailableException if the given language is
+     * not available
+     */
+    public void setLanguage(String lang) throws LanguageSelectedNotAvailableException {
         this.currentLanguage = lang;
     }
 }
