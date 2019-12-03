49a3f3080b8e51aac495ee50f32568fa3ba0826f
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-12-03 12:24:28 +0100

    Rinominazione FormatFileHandler in FilesInformations

--- unisalife/src/language/FilesInformations.java
+++ unisalife/src/language/FilesInformations.java
@@ -5,18 +5,21 @@ package language;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 /**
  *
  * @author Giuseppe De Simone
  */
-class FormatFileHandler {
+class FilesInformations {
 
     private static final String FORMAT = ".xml";
+    private static final String PATH = "..//lang";
 
     public static String getFORMAT() {
         return FORMAT;
+    }
+
+    public static String getPATH() {
+        return PATH;
     }
 
 }
