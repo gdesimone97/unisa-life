7cce982f1da90df71b369b22b8f9946d62f28565
idsparco97 <liovito97@hotmail.it>
2019-12-06 08:51:45 +0100

    Added modifications on run() method of Exam class

--- unisalife/src/exam/Exam.java
+++ unisalife/src/exam/Exam.java
@@ -9,6 +9,7 @@ import exam.question.*;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import unisagui.*;
 
 /**
  * This class is used to do an exam of a given subject.
@@ -21,6 +22,7 @@ public class Exam implements Runnable {
     private final Questions questions;
     private final Materia subject;
     private int answered;
+    private final int questionTime;
     private int score;
     private float sum;
     private int count;
@@ -40,6 +42,7 @@ public class Exam implements Runnable {
         this.score = 0;
         this.sum = 0;
         this.count = 0;
+        this.questionTime = 30;
         this.questions = questionsFetch.getQuestions();
         this.maxLevel = this.questions.getNumLevels();
         this.basicScore = 12/(30-(30/(float)(this.maxLevel-1)));
@@ -57,7 +60,7 @@ public class Exam implements Runnable {
      * for each answer
      */
     public void verifyAnswer(boolean answer, int seconds, int level){ //here the level is referred to the number of questions choosed
-        int x = (30-(30/(this.maxLevel-level))); //seconds from which the user loses points
+        int x = (this.questionTime-(this.questionTime/(this.maxLevel-level))); //seconds from which the user loses points
         if(level!=maxLevel && answer){
             this.sum += (seconds >= x ? 30 : 30-this.basicScore*(x-seconds));
         }else if(level!=maxLevel && !answer){
@@ -81,9 +84,9 @@ public class Exam implements Runnable {
      *
      * @return the sum of the score achieved during the exam
      */
-    public float getCurrentScore(){
-        return this.sum;
-    }
+//    public float getCurrentScore(){
+//        return this.sum;
+//    }
     
     public synchronized void setAnswered(int position){
         this.answered = position;
@@ -93,16 +96,18 @@ public class Exam implements Runnable {
      public synchronized int pause() {
         long start;
         long elapsed;
+        
+        System.out.println("Ciao porco dio bastardo");
          
         //beginning of the time passing
         start = System.nanoTime();
         try {
-            this.wait(30000);
+            this.wait(this.questionTime*1000);
         } catch (InterruptedException ex) {}
         finally{
             elapsed = System.nanoTime() - start;
         }
-        
+        System.out.println("Ciao porca madonna in croce");
         return (int) elapsed/1000000000;
      }
             
@@ -110,26 +115,31 @@ public class Exam implements Runnable {
     @Override
     public void run() {
         GuiManager gui = GuiManager.getInstance();
-        GuiManager.showExamDialog(this.subject.toString());
+        gui.showExamDialog(this.subject.toString(),true);
+        int elapsedTime;
+        Question question;
+        int j=1;
         
-        while(iter.hasNext()){
-            
-
-            Question question = iter.next();
-            GuiManager.setExamQuestion(question.getQuestion());
+        while(iter.hasNext() && j<4){
+            question = iter.next();
+            gui.setExamQuestion(question.getQuestion());
             ArrayList<Answer> answers = question.getAnswers();
             
             int i = 1;
             for(Answer a : answers){
-                GuiManager.setExamAnswer(a,i++);
+                try {
+                    gui.setExamAnswer(a.toString(),i++);
+                } catch (Exception ex) {}
             }
+            elapsedTime = pause();
             
+            boolean correctness = question.isCorrect(answers.get(this.answered-1));
             
-            
-            
-            
-            Answer answer = answers.get(Integer.parseInt(stringa)-1);
+            verifyAnswer(correctness,elapsedTime,question.getLevel());
+            j++;
         }
+        
+        System.out.println("Voto: "+ getScore());
     }
     
 }
