a717c652b09908e53d45f94200a3296b0bf560b5
Giuseppe <gds2011@libero.it>
2019-12-07 12:50:23 +0100

    fatto save metodo

--- unisalife/src/saving/SaveManager.java
+++ unisalife/src/saving/SaveManager.java
@@ -5,33 +5,55 @@
  */
 package saving;
 
+import saving.exceptions.*;
 import exam.booklet.BookletSingleton;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import language.TextManager;
+
 /**
  *
- * @author 
+ * @author
  */
 public class SaveManager {
+
     private static SaveManager instance = new SaveManager();
     private List<Saveable> saveableComponents = new ArrayList<>();
-    private Map<String,Serializable> savingItems = new HashMap<>();
+    private Map<String, Serializable> savingItems = new HashMap<>();
     private final String PATH = ""; // path per la cartella di salvataggio
+
+    public static SaveManager getSaveManager(){
+        return instance;
+    }
     
-    public SaveManager() {
+    private SaveManager() { // da completare quando abbiamo tutte le classi da salvare
         saveableComponents.add(BookletSingleton.getInstance());
     }
-    
-    public void save(){
-        
+
+    public void save() throws SavingException{
+        for (Saveable sav : saveableComponents) {
+            Serializable itemToSave = sav.save();
+            savingItems.put(sav.getClass().getName(), itemToSave);
+        }
+        try (FileOutputStream fileout = new FileOutputStream(new File(PATH));
+                ObjectOutputStream s = new ObjectOutputStream(fileout);
+                ){
+            s.writeObject(savingItems);
+            
+        } catch (IOException ex) {
+            throw new SavingException();
+        }
     }
-    
-    public void load(){
-        
+
+    public void load() {
+
     }
-    
+
 }
