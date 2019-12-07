ffe14201f5181e7452866b3c036ca636e5b54f35
Giuseppe <gds2011@libero.it>
2019-12-07 12:21:43 +0100

    Update SaveManager.java

--- unisalife/src/saving/SaveManager.java
+++ unisalife/src/saving/SaveManager.java
@@ -5,10 +5,26 @@
  */
 package saving;
 
+import exam.booklet.BookletSingleton;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import language.TextManager;
 /**
  *
  * @author 
  */
 public class SaveManager {
+    private static SaveManager instance = new SaveManager();
+    private List<Saveable> saveableComponents = new ArrayList<>();
+    private Map<String,Serializable> savingItems = new HashMap<>();
+    
+    public SaveManager() {
+        saveableComponents.add(BookletSingleton.getInstance());
+        
+        
+    }
     
 }
