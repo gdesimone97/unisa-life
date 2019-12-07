081169e25a6ff5dc09ccceac13362fdea881e350
Giuseppe <gds2011@libero.it>
2019-12-07 16:21:36 +0100

    Update Saveable.java

--- unisalife/src/saving/Saveable.java
+++ unisalife/src/saving/Saveable.java
@@ -11,17 +11,24 @@ import saving.exceptions.*;
 /**
  * This interface declares two methods in order to perform the saving and
  * loading operations
- * @author 
+ *
+ * @author
  */
 public interface Saveable {
+
     /**
      * This method have to be implemented by sub-classes.
-     * @return seriarizable object containig the objects that sub-classes want to save
+     *
+     * @return seriarizable object containig the objects that sub-classes want
+     * to save
      */
     public Serializable save();
+
     /**
      * This method have to be implemented by sub-classes.
-     * @param obj containing the object that sub-class has saved and they want load it
+     *
+     * @param obj containing the object that sub-class has saved and they want
+     * load it
      * @throws LoadingException if some errors occur during the loading process
      */
     public void load(Serializable obj) throws LoadingException;
