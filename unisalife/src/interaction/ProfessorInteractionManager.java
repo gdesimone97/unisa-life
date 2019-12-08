a0b4cfc82947c6d5e6372fdf1d7c92100a7104f2
GiackV <1997gennaro@gmail.com>
2019-12-08 20:01:10 +0100

    professor

--- unisalife/src/interaction/ProfessorInteractionManager.java
+++ unisalife/src/interaction/ProfessorInteractionManager.java
@@ -5,6 +5,8 @@
  */
 package interaction;
 
+import exam.Exam;
+import exam.question.Materia;
 import game.Interfaces.Interactable;
 
 /**
@@ -15,6 +17,15 @@ public class ProfessorInteractionManager implements InteractionManager {
 
     @Override
     public void execute(Interactable obj) {
+        // 1. trova la materia giusta in base al prof (il prof deve avere un attributo materia)
+        
+        // 2. verifica idonietà e requisiti
+        
+        // 3. Start the exam session
+        // Thread esameThread = new Thread(new Exam(Materia.matematica));
+        // esameThread.start();
+        
+        // 4. modifica stato e ricompense
     }
     
 }
