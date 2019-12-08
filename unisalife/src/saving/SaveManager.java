84ff4297d68ce2a2caf4647d3d6252a054b5ae3b
Giuseppe <gds2011@libero.it>
2019-12-08 15:44:49 +0100

    Update SaveManager.java

--- unisalife/src/saving/SaveManager.java
+++ unisalife/src/saving/SaveManager.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import language.adapters.TextManagerAdapter;
 
 /**
  *
@@ -27,26 +28,34 @@ public class SaveManager {
     private static SaveManager instance = new SaveManager();
     private List<Saveable> saveableComponents = new ArrayList<>();
     private Map<String, Serializable> savingItems = new HashMap<>();
-    private final String PATH = ""; // path per la cartella di salvataggio
+    private final String PATH = "../save/save.game"; // path per la cartella di salvataggio
 
-    public static SaveManager getSaveManager(){
+    public static SaveManager getSaveManager() {
         return instance;
     }
-    
+
     private SaveManager() { // da completare quando abbiamo tutte le classi da salvare
         saveableComponents.add(BookletSingleton.getInstance());
+        saveableComponents.add(TextManagerAdapter.getTextManagerAdpter());
     }
 
-    public void save() throws SavingException{
+    public boolean isSaveSomething() {
+        File f = new File(PATH);
+        if (!f.exists() || f.getTotalSpace() == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public void save() throws SavingException {
         for (Saveable sav : saveableComponents) {
             Serializable itemToSave = sav.save();
             savingItems.put(sav.getClass().getName(), itemToSave);
         }
         try (FileOutputStream fileout = new FileOutputStream(new File(PATH));
-                ObjectOutputStream s = new ObjectOutputStream(fileout);
-                ){
+                ObjectOutputStream s = new ObjectOutputStream(fileout);) {
             s.writeObject(savingItems);
-            
+
         } catch (IOException ex) {
             throw new SavingException();
         }
