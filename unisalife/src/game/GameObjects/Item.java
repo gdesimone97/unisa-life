87a0e9262fc856579a36c9317c99ae1a2a8383f0
Giuseppe <gds2011@libero.it>
2019-12-10 14:26:55 +0100

    interact Item da controllare

--- unisalife/src/game/GameObjects/Item.java
+++ unisalife/src/game/GameObjects/Item.java
@@ -4,9 +4,10 @@
  * and open the template in the editor.
  */
 package game.GameObjects;
+
 import game.Interfaces.Interactable;
 import game.Interfaces.Renderable;
-import game.Interfaces.Information;
+import language.Information;
 import java.awt.Graphics;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -14,62 +15,67 @@ import java.time.LocalDateTime;
 import java.io.Serializable;
 import java.time.temporal.ChronoUnit;
 import java.util.Objects;
+import language.FileTextManager;
+import language.TextManager;
+import unisagui.GuiManager;
+
 /**
  *
  * @author simon
  */
-public class Item extends GameObject implements Renderable,Interactable,Serializable,Comparable<Item>,Information {
+public class Item extends GameObject implements Renderable, Interactable, Serializable, Comparable<Item>, Information {
+
     private final String title;
     private final String info;
     private BufferedImage facingDownImage;
     private LocalDateTime taken;
 
-    
-    public Item(float x,float y,ObjectId i,String path,String title,String info){
-        super(x,y,i);
-        this.title=title;
-	this.info=info;
+    public Item(float x, float y, ObjectId i, String path, String title, String info) {
+        super(x, y, i);
+        this.title = title;
+        this.info = info;
         try {
-        facingDownImage = ImageIO.read(
-				getClass().getResourceAsStream(path)
-        );}
-        catch (Exception e) {
+            facingDownImage = ImageIO.read(
+                    getClass().getResourceAsStream(path)
+            );
+        } catch (Exception e) {
             System.exit(1);
+        }
     }
-    }
-        @Override
-	public boolean equals( Object o){
-            if(o==null||!(o instanceof Item))
-                return false;
-        return this.info.equals(((Item)o).getInfo());
-
-}
 
     @Override
-    public int compareTo(Item o){
-        if(o==null)
-            return 1;
-       return info.compareTo(o.getInfo());
-}
+    public boolean equals(Object o) {
+        if (o == null || !(o instanceof Item)) {
+            return false;
+        }
+        return this.info.equals(((Item) o).getInfo());
 
-    public void setTaken(){
-        this.taken=LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
     }
-    
+
+    @Override
+    public int compareTo(Item o) {
+        if (o == null) {
+            return 1;
+        }
+        return info.compareTo(o.getInfo());
+    }
+
+    public void setTaken() {
+        this.taken = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+    }
+
     public String getTitle() {
         return title;
     }
 
     @Override
-   public String getInfo(){
-	return this.info;
-}
-	
+    public String getInfo() {
+        return this.info;
+    }
+
     public LocalDateTime getTaken() {
         return taken;
     }
-
-   
 
     @Override
     public int hashCode() {
@@ -77,23 +83,34 @@ public class Item extends GameObject implements Renderable,Interactable,Serializ
         hash = 31 * hash + Objects.hashCode(this.info);
         return hash;
     }
-        
-    
-    @Override
-    public String toString(){
-        return "Object : " + this.title + " ( taken in "+ this.taken +" )";
-    }
-    
-    
-   @Override
-    public void render(Graphics g){
-        g.drawImage(facingDownImage, (int)x, (int)y, width, height, null);
-    }
-    
-    @Override
-    public void interact(){
-        System.out.println("hai raccolto "+title);
-    }
-    
-}
 
+    @Override
+    public String toString() {
+        return "Object : " + this.title + " ( taken in " + this.taken + " )";
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(facingDownImage, (int) x, (int) y, width, height, null);
+    }
+
+    @Override
+    public void interact() {
+        /*
+        try {
+        FileTextManager textManager = FileTextManager.getFileTextManager();
+        String s = textManager.getString(this).get(0);
+        GuiManager guiManager = GuiManager.getInstance();
+        guiManager.showDialog(s);
+        } catch (Exception ex){
+            System.out.println("Errore");
+        }
+        */
+        System.out.println("hai raccolto " + title);
+    }
+
+    @Override
+    public Boolean isAvailable() {
+        return true;
+    }
+}
