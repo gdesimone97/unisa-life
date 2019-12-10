e165b4813dd5f801f60afb27d08a06bfbdf47674
Giuseppe <gds2011@libero.it>
2019-12-10 15:05:22 +0100

    Update GameObject.java

--- unisalife/src/game/GameObjects/GameObject.java
+++ unisalife/src/game/GameObjects/GameObject.java
@@ -4,100 +4,89 @@
  * and open the template in the editor.
  */
 package game.GameObjects;
+
 import game.GameResources.Game;
 import java.awt.Rectangle;
+
 /**
  * represents an object of the game(player, character, block, teleport, item).
+ *
  * @author simon
  */
 public abstract class GameObject {
-    protected float x,y;
-    
-    protected ObjectId id;
-    protected int width=Game.DIMENSIONSPRITE;
-    protected int height=Game.DIMENSIONSPRITE;
-    
+
+    protected float x, y;
+
+    protected int width = Game.DIMENSIONSPRITE;
+    protected int height = Game.DIMENSIONSPRITE;
+
     /**
      *
      * @param x
      * @param y
-     * @param id
      */
-    public GameObject(float x,float y, ObjectId id){
-        this.x=x;
-        this.y=y;
-        this.id=id;
+    public GameObject(float x, float y) {
+        this.x = x;
+        this.y = y;
     }
-    
+
     /**
      *
      * @param object
      */
     //public abstract void tick(LinkedList<GameObject> object);
-
     /**
      *
      * @param g
      */
     //public abstract void render(Graphics g);
-    
     //methods getter and setter  
+    /**
+     *
+     * @return
+     */
+    public float getX() {
+        return x;
+    }
 
     /**
      *
      * @return
      */
-        public float getX(){
-        return x;
-    }
-    
-    /**
-     *
-     * @return
-     */
-    public  float getY(){
+    public float getY() {
         return y;
-        
+
     }
-    
+
     /**
      *
      * @param x
      */
-    public void setX(float x){
-        this.x=x;
+    public void setX(float x) {
+        this.x = x;
     }
-    
+
     /**
      *
      * @param y
      */
-    public void setY(float y){
-        this.y=y;
+    public void setY(float y) {
+        this.y = y;
     }
-    
-    /**
-     *
-     * @return
-     */
-    public ObjectId getId() {
-        return this.id;
-    }
+
     /*public int getHeight(){
         return this.height;
     }
     public int getWidth(){
         return this.width;
     }
-    */
-
+     */
     /**
      *
      * @return
      */
-    
-    public Rectangle getBounds(){
-        return new Rectangle((int)x,(int)y,(int)width,(int)height);
+    public Rectangle getBounds() {
+        return new Rectangle((int) x, (int) y, (int) width, (int) height);
     }
-    
+
 }
