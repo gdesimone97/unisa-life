e4e281538b17ed3f20f099d646184c8fb58cbfda
GiackV <1997gennaro@gmail.com>
2019-12-11 20:47:58 +0100

    cambio nome metodo

--- unisalife/src/exam/Exam.java
+++ unisalife/src/exam/Exam.java
@@ -109,7 +109,7 @@ public class Exam implements Runnable {
         return this.sum;
     }
 
-    private boolean isPraiseAvailable() {
+    private boolean isDistinctionAvailable() {
         return lastLevelAnswered == maxLevel - 1;
     }
 
@@ -128,8 +128,8 @@ public class Exam implements Runnable {
 
         while (iter.hasNext()) {
 
-            if (isPraiseAvailable()) {
-                //Praise Question
+            if (isDistinctionAvailable()) {
+                //Distinction Question
                 if (getCurrentScore() != (maxLevel-1)*30) {
                     iter.next();
                     continue;
