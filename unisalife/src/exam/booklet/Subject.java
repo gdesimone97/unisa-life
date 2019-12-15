cb8a4051ef1b7d270ce8e074b8bc4b13f523548b
fonzdm <33768748+fonzdm@users.noreply.github.com>
2019-12-15 16:46:49 +0100

    Uodate subject, deleted Materia

--- unisalife/src/exam/booklet/Subject.java
+++ unisalife/src/exam/booklet/Subject.java
@@ -4,29 +4,34 @@
  * and open the template in the editor.
  */
 package exam.booklet;
+
 import java.io.Serializable;
+import language.Information;
 
 /**
- * This class is used to give a structure for storing a score of an exam and
- * its availabilty
- * 
+ * This class is used to give a structure for storing a score of an exam and its
+ * availabilty
+ *
  * @author liovi
  */
-public class Subject implements Serializable{
+public class Subject implements Information, Serializable {
+
     private int score;
-    private boolean available; 
+    private String subject;
+    private boolean available;
 
     /**
      * Constructor that instantiates the object and sets it as available
      */
-    public Subject() {
+    public Subject(String subject) {
+        this.subject = subject;
         this.score = 0;
         this.available = true;
     }
 
     /**
      * This method sets the score of corresponding exam
-     * 
+     *
      * @param score the score reached at the exam
      */
     public void setScore(int score) {
@@ -35,29 +40,38 @@ public class Subject implements Serializable{
 
     /**
      * This method is used to set an exam no longer available if its passed
-     * 
+     *
      * @param available boolean used to indicate availability
      */
     public void setAvailable(boolean available) {
         this.available = available;
     }
-    
+
     /**
      * This method gives the state of the exam
-     * 
+     *
      * @return a boolean that indicates if the exam is or not is available
      */
-    public boolean isAvailable(){
+    public boolean isAvailable() {
         return this.available;
     }
 
     /**
      * This method returns the corrispondig score of the exam
-     * 
+     *
      * @return an integer that indicates the reached score
      */
     public int getScore() {
         return score;
     }
-    
+
+    @Override
+    public String toString() {
+        return subject;
+    }
+
+    @Override
+    public String getInfo() {
+        return subject;
+    }
 }
