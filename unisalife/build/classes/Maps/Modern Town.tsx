c8f196366c8e8545aab268e0eb95c6136a40472a
idsparco97 <liovito97@hotmail.it>
2019-12-18 15:26:54 +0100

    Small modifications

--- unisalife/build/classes/Maps/Modern Town.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="Modern Town" tilewidth="32" tileheight="32" tilecount="432" columns="8">
- <image source="../Tilesets/Modern Town.png" width="256" height="1728"/>
-</tileset>
---
+++ unisalife/src/gameSystem/map/MappaObjects.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameSystem.map;
+
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author liovi
+ */
+public class MappaObjects extends MappaMia{
+    private Sprite sprite;
+    private List<Sprite> sprites;
+    private int drawSpritesAfterLayer = 1;
+
+    public MappaObjects(TiledMap map) {
+        super(map);
+        sprites = new ArrayList<Sprite>();
+    }
+
+    public void addSprite(Sprite sprite){
+        sprites.add(sprite);
+    }
+
+    @Override
+    public void render() {
+        beginRender();
+        int currentLayer = 0;
+        for (MapLayer layer : map.getLayers()) {
+            if (layer.isVisible()) {
+                if (layer instanceof TiledMapTileLayer) {
+                    renderTileLayer((TiledMapTileLayer)layer);
+                    currentLayer++;
+                    if(currentLayer == drawSpritesAfterLayer){
+                        for(Sprite sprite : sprites)
+                            sprite.draw(this.getSpriteBatch());
+                    }
+                } else {
+                    for (MapObject object : layer.getObjects()) {
+                        renderObject(object);
+                    }
+                }
+            }
+        }
+        endRender();
+    }
+}
