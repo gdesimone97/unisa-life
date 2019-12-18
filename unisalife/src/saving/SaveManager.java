ad9aab2120001ecfdc98b4aba0baa5fe1cfddcf7
Giuseppe <gds2011@libero.it>
2019-12-18 18:26:41 +0100

    Update SaveManager.java

--- unisalife/src/saving/SaveManager.java
+++ unisalife/src/saving/SaveManager.java
@@ -43,6 +43,7 @@ public class SaveManager {
 
     private SaveManager() { // da completare quando abbiamo tutte le classi da salvare
         saveableComponents.add(TextManagerAdapter.getTextManagerAdpter());
+        saveableComponents.add(MapManager.getInstance());
         saveableComponents.add(BookletSingleton.getInstance());
         saveableComponents.add(Player.getIstance());
         saveableComponents.add(GameInventorySingleton.getInstance());
