0072c168d12a6e1c230e69dd38103dd672cac191
GiackV <1997gennaro@gmail.com>
2019-12-19 15:50:06 +0100

    Versione Funzionante

--- unisalife/src/gameSystem/LoadingState.java
+++ unisalife/src/gameSystem/LoadingState.java
@@ -5,12 +5,17 @@
  */
 package gameSystem;
 
+import game.Interfaces.Initializable.InitException;
 import gameSystem.keySettings.KeyCommand;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -19,17 +24,25 @@ import java.util.logging.Logger;
 public class LoadingState extends GameState{
     
     private static LoadingState instance;
+    private BufferedImage img;
     
-    public static LoadingState getInstance() {
+    public static LoadingState getInstance() throws InitException {
         if (instance == null) {
             instance = new LoadingState();
         }
         return instance;
     }
     
+    private LoadingState() throws InitException {
+        try {
+            img = ImageIO.read(getClass().getResource("/Sprites/loadingwindow.png"));
+        } catch (IOException ex) {
+            throw new InitException("Can't find Loading image");
+        }
+    }
+    
     @Override
     public void init() {
-        
     }
 
     @Override
@@ -39,11 +52,13 @@ public class LoadingState extends GameState{
 
     @Override
     public void render(Graphics2D g) {
-        g.setColor(Color.black);
-        g.fillRect(0, 0, Game.WIDTHSCREEN, Game.HEIGHTSCREEN2);
-        g.setColor(Color.white);
-        g.setFont(new Font("TimesRoman", Font.PLAIN, 30));
-        g.drawString("Loading..", Game.WIDTHSCREEN/2, Game.HEIGHTSCREEN2/2);
+        AffineTransform at = new AffineTransform();
+        g.drawImage(img, at, null);
+//        g.setColor(Color.black);
+//        g.fillRect(0, 0, Game.WIDTHSCREEN, Game.HEIGHTSCREEN2);
+//        g.setColor(Color.white);
+//        g.setFont(new Font("TimesRoman", Font.PLAIN, 30));
+//        g.drawString("Loading..", Game.WIDTHSCREEN/2, Game.HEIGHTSCREEN2/2);
     }
 
     @Override
