05dd49c6d5da8425b124c766f22495f4d400c0fa
GiackV <1997gennaro@gmail.com>
2019-12-30 14:36:17 +0100

    stati

---
+++ unisalife/src/gameSystem/EndGameState.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameSystem;
+
+import game.Interfaces.Initializable;
+import gameSystem.keySettings.interfaces.KeyCommand;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author 1997g
+ */
+public class EndGameState extends GameState {
+    
+    private static EndGameState instance;
+    private BufferedImage img;
+    
+    public static EndGameState getInstance() {
+        if (instance == null) {
+            instance = new EndGameState();
+        }
+        return instance;
+    }
+    
+    private EndGameState() {
+    }
+
+    @Override
+    public void init() throws Initializable.InitException {
+        //SETTARE IMMAGINE
+//        try {
+//            img = ImageIO.read(getClass().getResource("/Sprites/AGGIUNGEREIMMAGINE.png"));
+//        } catch (IOException ex) {
+//            throw new Initializable.InitException("Can't find EndGame image");
+//        }
+    }
+
+    @Override
+    public void tick() {
+    }
+
+    @Override
+    public void render(Graphics2D g) {
+        g.drawImage(img, 0, 0, null);
+    }
+
+    @Override
+    public void handleInput(KeyCommand cmd) {
+    }
+    
+}
