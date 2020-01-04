c777279d2ed8292a14cf3d597b6223736857815c
WhiteZeus97 <58167610+WhiteZeus97@users.noreply.github.com>
2020-01-04 16:18:06 +0100

    Aggiunti test parziali con JUnit ( su alcuni elementi della gui). Aggiunto menu delle lingue con accesso al database delle lingue. Aggiunte righe al libretto.

--- unisalife/test/unisagui/GameFrameTest.java
+++ unisalife/test/unisagui/GameFrameTest.java
@@ -5,16 +5,20 @@
  */
 package unisagui;
 
+import java.awt.event.KeyEvent;
+import static java.lang.Thread.sleep;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import javax.swing.*;
 
 /**
  *
  * @author Davide
+ * Test for some classes and component of the gui
  */
 public class GameFrameTest {
     
@@ -42,61 +46,119 @@ public class GameFrameTest {
      */
     @Test
     public void testGetInstance() {
-        System.out.println("getInstance");
         GameFrame expResult = null;
         GameFrame result = GameFrame.getInstance();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        assertNotEquals(expResult,result);
+        
     }
 
     /**
      * Test of setCareer method, of class GameFrame.
-     */
+     
     @Test
     public void testSetCareer() {
         System.out.println("setCareer");
         GameFrame instance = null;
         instance.setCareer();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        if(true)
+            System.out.println(" TESTING SET CAREER  OK");
+        else
+            System.out.println("TESTING SET CAREER  FAILED");
+        
     }
+    */
 
     /**
      * Test of initializingTable method, of class GameFrame.
-     */
+     
     @Test
     public void testInitializingTable() {
         System.out.println("initializingTable");
         GameFrame instance = null;
         instance.initializingTable();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
+    */
 
-    /**
-     * Test of settingLanguage method, of class GameFrame.
-     */
-    @Test
-    public void testSettingLanguage() throws Exception {
-        System.out.println("settingLanguage");
-        String s = "";
-        GameFrame instance = null;
-        instance.settingLanguage(s);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
+    
+    
     /**
      * Test of main method, of class GameFrame.
-     */
+     
     @Test
     public void testMain() {
-        System.out.println("main");
         String[] args = null;
         GameFrame.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+    }
+    * */
+    
+
+    
+    @Test
+    public void testAvatarName() throws InterruptedException {
+        GameFrame frame;
+        JDialog temp;
+        String result;
+        JTextField inputTest;
+
+        frame = GameFrame.getInstance();
+        frame.setVisible(true);
+        temp = frame.AvatarChooserDialog;
+        temp.setVisible(true);
+
+        inputTest = (JTextField) TestUtils.getChildNamed(temp, "avatar");
+        assertNotNull("Cant'access the JTextField component", inputTest);
+
+        inputTest.setText("davide");
+        sleep(1000);
+        inputTest.postActionEvent();
+        sleep(1000);
+    
+
+        result = "davide";
+        
+        assertEquals(result,inputTest.getText());
+    }
+    
+    @Test
+    public void testnewGameButton() throws InterruptedException {
+        GameFrame frame;
+        JDialog temp;
+        boolean result;
+        JButton button;
+
+        frame = GameFrame.getInstance();
+        frame.setVisible(true);
+        temp = frame.MainMenuDialog;
+        temp.setVisible(true);
+
+        button = (JButton) TestUtils.getChildNamed(temp, "new_game");
+        assertNotNull("Cant'access the JButton component", button);
+
+        button.setEnabled(false);
+        sleep(1000);
+
+        result = false;
+        assertEquals(result,button.isEnabled());
+    }
+    
+    @Test
+    public void testKeyboard() throws Exception {
+        String moveuptest= "W";
+        JTextField inputTest;
+        GameFrame frame= GameFrame.getInstance();
+        JDialog temp=frame.KeyboardSettingsDialog;
+     
+        frame.setKeyBoard();
+        sleep(1000);
+        temp.setVisible(true);
+        inputTest = (JTextField) TestUtils.getChildNamed(temp,"moveup");
+        assertNotNull("Cant'access the JTextField component", inputTest);
+        
+        assertEquals(moveuptest,inputTest.getText()); 
+        
     }
     
 }
